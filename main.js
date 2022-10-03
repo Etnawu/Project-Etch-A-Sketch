fdfda86b7f0dc074ee49af0248c8902f6366c480
Ethan <wuethann@gmail.com>
2022-10-03 19:18:15 -0400

    Grid slider function

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-grid = 16
+function populateBoard(size) {
+    let board = document.querySelector("#squareContainer");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div => div.remove()));
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for (let i = 0; i < grid*grid; i++){
 
-let square = document.createElement('div');
-square.className = "square";
-document.getElementById("squareContainer").appendChild(square);
-square.addEventListener("mouseover", change => change.target.classList.add('coloredSquare'));
 
+
+
+    let grid = size*size;
+    for (let i = 0; i < grid; i++){
+        let square = document.createElement('div');
+        square.className = "square";
+        document.getElementById("squareContainer").appendChild(square);
+        square.addEventListener("mouseover", change => change.target.classList.add('coloredSquare'));
+    }
 }
 
+populateBoard(16)
 
 
-
-
-
-
-
-
-
+function changeSize(input) {
+    populateBoard(input)
+}
 
 
 
