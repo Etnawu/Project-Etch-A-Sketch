787bbfb10d2b62301b5e88421d5f7ce4a9e1a5d0
Ethan <wuethann@gmail.com>
2022-10-02 22:30:47 -0400

    Made functioning etch a sketch

--- main.js
+++ main.js
@@ -3,15 +3,10 @@ for (let i = 0; i < 256; i++){
 let square = document.createElement('div');
 square.className = "square";
 document.getElementById("squareContainer").appendChild(square);
-square.addEventListener("mouseover", draw, false);
 
-function draw(){
-    
-    square = document.getElementsByClassName("square");
-    square.SetAttribute("style","background:blue;")
-    return
+square.addEventListener("mouseover",  
+    change => change.target.classList.add('coloredSquare')
+);
+
 }
-}
-
-
 
