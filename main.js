b632386719c6d798a96637ecd256edf891de0281
Ethan <wuethann@gmail.com>
2022-10-03 17:54:16 -0400

    Add clear all button

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
-for (let i = 0; i < 256; i++){
+grid = 16
+
+for (let i = 0; i < grid*grid; i++){
 
 let square = document.createElement('div');
 square.className = "square";
 document.getElementById("squareContainer").appendChild(square);
-
-square.addEventListener("mouseover",  
-    change => change.target.classList.add('coloredSquare')
-);
+square.addEventListener("mouseover", change => change.target.classList.add('coloredSquare'));
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function clearAllColor() {
+    let square = squareContainer.querySelectorAll('div');
+    square.forEach(change => change.classList.remove('coloredSquare'));
+}
+
+
+let clearBtn = document.getElementById("clearAll");
+clearBtn.addEventListener("click", clearAllColor);
+
 
