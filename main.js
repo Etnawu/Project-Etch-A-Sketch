7441075b9f1aa7910f6cda2fd9dae570557a89cd
Ethan <wuethann@gmail.com>
2022-10-03 20:27:41 -0400

    Fixed button press bug

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
+//function to make squares in square container
 function populateBoard(size) {
     let board = document.querySelector("#squareContainer");
     let squares = board.querySelectorAll("div");
     squares.forEach((div => div.remove()));
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-
-
-
 
     let grid = size*size;
     for (let i = 0; i < grid; i++){
@@ -18,28 +15,20 @@ function populateBoard(size) {
     }
 }
 
+//state of board when the site is first opened
 populateBoard(16)
 
-
+//function to change square container size
 function changeSize(input) {
     populateBoard(input)
 }
 
-
-
-
-
-
-
-
-
+//function to erase everything
 function clearAllColor() {
     let square = squareContainer.querySelectorAll('div');
     square.forEach(change => change.classList.remove('coloredSquare'));
 }
 
-
 let clearBtn = document.getElementById("clearAll");
 clearBtn.addEventListener("click", clearAllColor);
-
 
